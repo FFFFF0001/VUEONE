7dbf533921dd15f572993da7eff85475e02b5a3e
coderxuan <mifind@sina.com>
2017-08-09 14:00:16 +0800

    sth commit

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,7 +5,6 @@ var config = require('../config')
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
@@ -42,42 +41,6 @@ var webpackConfig = merge(baseWebpackConfig, {
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin(),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    // new HtmlWebpackPlugin({
-    //   filename: config.build.index,
-    //   template: 'index.html',
-    //   inject: true,
-    //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //     removeAttributeQuotes: true
-    //     // more options:
-    //     // https://github.com/kangax/html-minifier#options-quick-reference
-    //   },
-    //   // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-    //   chunksSortMode: 'dependency'
-    // }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          (
-            module.resource.indexOf(
-              path.join(__dirname, '../node_modules')
-            ) === 0 ||
-            module.resource.indexOf(
-              path.join(__dirname, '../static/libs')
-            ) === 0
-          )
-        )
-      }
-    }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
---
+++ src/module/test/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="test">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test'
+  }
+</script>
+
+<style>
+  #app {
+    color: #2c3e50;
+  }
+</style>
